46c45b09de68cbd9836e958642243d13fe5c82f2
Matiouvina <50047072@est.ort.edu.ar>
2026-06-03 09:05:59 -0300

    Intento punto 3, falta interactivearea

---
+++ New Unity Project/Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+    public int score = 0;
+    
+    private UIManager uiManager;
+
+    void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            score++;
+            uiManager.UpdateScore(score);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+     public TextMeshProUGUI score;
+     public TextMeshProUGUI timer;
+
+    public void UpdateScore (int currentScore)
+    {
+        score.text = "Score: " + currentScore;
+    }
+
+    public void UpdateTimer(float currentTime)
+    {
+        timer.text = "0:" + Mathf.CeilToInt(currentTime).ToString("00");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
